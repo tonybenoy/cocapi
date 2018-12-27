fb65964b58ee08918fb06c01c6d21465397a1904
Tony Benoy <tonybenoy@gmail.com>
2018-12-27 21:37:50 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ClashOfClansAPI",
-    version="0.0.1",
+    version="0.0.2",
     author="Tony Benoy",
     author_email="me@tonybenoy.com.com",
     description="A python wrapper around clash of clans api",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
     ),
 )
